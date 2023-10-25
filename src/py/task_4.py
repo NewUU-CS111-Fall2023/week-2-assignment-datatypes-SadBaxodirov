c5ea12a456a1313a77c5abf8fae67acb9aee52cf
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2023-10-25 22:06:58 +0500

    Delete src/py/task_4.py

--- src/py/task_4.py
+++
@@ -1,9 +0,0 @@
-# * Author:
-# * Date:
-sent = str(input("Input a sentence:"))
-word = str(input("Input a word:"))
-words = sent.split()
-if (word in words):
-    print(words.index(word))
-else:
-    print("no match")
