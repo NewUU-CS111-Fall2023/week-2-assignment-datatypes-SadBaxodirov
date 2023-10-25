69994580e7b3ea2fbc5506896f373dbfb039d25c
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2023-10-25 22:31:18 +0500

    Update task_1.h

--- src/cpp/task_1.h
+++ src/cpp/task_1.h
@@ -1,5 +1,19 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+#include <cstdlib>
+
+void task_1(int year){
+    if (year%4==0){
+        if(year%100==0){
+            if (year%400==0){
+                std::cout<<"true"<<std::endl;
+            } else{
+                std::cout<<"false"<<std::endl;
+            }
+        } else{
+            std::cout<<"true"<<std::endl;
+        }
+    } else{
+        std::cout<<"false"<<std::endl;
+    }
+    
+}
